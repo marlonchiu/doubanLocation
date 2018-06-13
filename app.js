3741110d5e8cc555dc78b79e8cbfac55030b6004
marlonchiu <knight1898@outlook.com>
2018-06-13 11:13:15 +0800

    pages

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
   globalData: {
-    userInfo: null
+    userInfo: undefined,
+    latitude: undefined,
+    longitude: undefined,
+    windowWidth: undefined,
+    windowHeight: undefined,
+    locs: undefined,
+    currentLoc: undefined,
+    city: undefined,
+    reflesh: false,
+    locId: undefined,
+    eventCategory: undefined,
+    slogan: "在这陌生的城市里能与你相遇，真好",
+    doubanBase: "https://api.douban.com",
+    loc_list_url: "/v2/loc/list",
+    loc_url: "/v2/loc/",
+    event_list_url: "/v2/event/list",
+    event_url: "/v2/event/"
+  },
+  onLaunch: function () {
+    // Do something initial when launch.
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.globalData.windowWidth = res.windowWidth;
+        that.globalData.windowHeight = res.windowHeight;
+      }
+    });
+  },
+  onShow: function () {
+    // Do something when show.
+  },
+  onHide: function () {
+    // Do something when hide.
+  },
+  onError: function (msg) {
+    console.log(msg)
+  },
+  getUserInfo: function (cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo);
+    } else {
+      wx.login({
+        success: function (res) {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo);
+            }
+          });
+        }
+      });
+    }
+  },
+  getLocation: function () {
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        that.globalData.latitude = res.latitude;
+        that.globalData.longitude = res.longitude;
+      }
+    })
   }
 })
